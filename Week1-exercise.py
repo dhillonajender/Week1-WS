e4707f328264c355a4e24f5588fd1f99f223b584
dhillonajender <97201745+dhillonajender@users.noreply.github.com>
2022-05-03 19:42:07 -0400

    Add files via upload

---
+++ Week1-exercise.py
@@ -0,0 +1,7 @@
+input1=input("Enter your first input=")
+input2=input("Enter your second input=")
+a1= int(input1) + int(input2)
+a2= int(input1) - int(input2)
+a3= int(input1) * int(input2)
+a4= int(input1) / int(input2)
+print("Sum= %d, Subtraction=%d, Multiplicaation= %d, Division= %d " % (a1, a2, a3, a4))
